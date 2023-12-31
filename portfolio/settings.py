ddf39236704e024c4d48e7637119bac4d7cac0f7
Stanley Omezie <85222393+stanace10@users.noreply.github.com>
2023-12-31 18:08:47 +0100

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,20 +77,11 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
-'''
-
-import dj_database_url
-
-DATABASES = {
-
-    'default': dj_database_url.parse(env('DATABASE_URL'))
 }
 
 # Password validation
